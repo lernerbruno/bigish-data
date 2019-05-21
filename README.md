48228ba3675054fe94e14f355b115688d6d167cb
Bruno Lerner <bru.lerner@gmail.com>
2019-05-21 11:20:00 +0300

    comparing times of execution - serial, parallel(thread), parallel(process)

---
+++ README.md
@@ -0,0 +1,13 @@
+# Big-ish data
+Workshop with Mike Tebeka
+
+### Parallelism vs Concurrency
+You can have 1 worker doing a lot of stuff at the same time -> this is Concurrency
+You can have a lot of workers doing stuff at the same time -> this is parallelism
+
+But there is a point where you just can't parallelize (e.g. wine making - you have to wait for it to be ready at some 
+point in the process)
+
+
+####Amdahl's Law
+,https://en.wikipedia.org/wiki/Amdahl%27s_law
--- sha256.py
+++ sha256.py
@@ -1,12 +1,62 @@
 from hashlib import sha256
+from time import monotonic
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+
 
 def file_sha256(path):
     """Return the sha256 of the file in path"""
     h = sha256()
     with open(path, 'rb') as fp:
         while True:
-            data = fp.read(10*1024)
+            data = fp.read(10 * 1024)
             if not data:
                 break
             h.update(data)
         return h.hexdigest()
+
+
+def parse_signature_file(path):
+    files = []
+    with open(path) as fp:
+        for line in fp:
+            signature, name = line.split()  # unpacking
+            files.append((name, signature))
+    return files
+
+
+def serial(signatures):
+    for name, signature in signatures:
+        sig = file_sha256(name)
+        if sig != signature:
+            # print(name)
+            pass
+
+
+def parallel(signatures, pool):
+    files = [name for name, _ in signatures]
+    calculated_sigs = pool.map(file_sha256, files)
+    for (name, signature), calculated in zip(signatures, calculated_sigs):
+        if signature != calculated:
+            print(name)
+
+
+root_dir = 'taxi'
+signatures = parse_signature_file(f'{root_dir}/sha256sum.txt')
+signatures = [(f'{root_dir}/{name}', sig) for name, sig in signatures]
+
+start = monotonic()
+serial(signatures)
+duration = monotonic() - start
+print(f'serial took {duration:.2f} sec')
+
+pool = ThreadPoolExecutor()
+start = monotonic()
+parallel(signatures, pool)
+duration = monotonic() - start
+print(f'parallel (thread pool) took {duration:.2f} sec')
+
+pool = ProcessPoolExecutor()
+start = monotonic()
+parallel(signatures, pool)
+duration = monotonic() - start
+print(f'parallel (process pool) took {duration:.2f} sec')
